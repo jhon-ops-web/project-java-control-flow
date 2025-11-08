df5a0c390f83e8090358ac00705ef0776f3e2045
jhon correa <jhoncorrea327@gmail.com>
2025-11-07 20:03:14 -0500

    feat: Trabajando con Bucles For

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,14 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Multiplication table for " + number + ":");
+
+        // TODO: Add a for loop here to print the multiplication table
+        for (int i = 1; i <= 10; i++) {
+    int result = number * i;
+    System.out.println(number + " x " + i + " = " + result);
+}
+
+    }
+}
+
